d38961b4c2cf5f5b5cda50b730fbd0469b925b51
Adam Mulligan <adammulligan@gmail.com>
2013-04-30 17:30:23 +0100

    Added arrested development countdown

--- lib/kindle/display.sh
+++ lib/kindle/display.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 rm output.png
 
-if wget -O output.png http://192.168.0.20:4000/image/index; then
+if wget -O output.png http://kindle.kobayashi.cyanoryx.com/image/index; then
   eips -c
   eips -c
   eips -g output.png
