f3e753ff087f60f6a2efb0c9ad4e84e6ea66cc25
Adam Mulligan <adammulligan@gmail.com>
2013-05-02 23:16:41 +0100

    Added weather display (currently not attached to anything)

--- app/app.rb
+++ app/app.rb
@@ -18,6 +18,12 @@ class Oblong < Padrino::Application
     @lastfm_track["name"] = "#{@lastfm_track["name"][0,28]}.." if @lastfm_track["name"].length > 28
     @lastfm_track["image"] = @lastfm_track["image"][2]["content"]
     @lastfm_track["artist"] = @lastfm_track["artist"]["content"]
+
+    #wunderground_url = "http://api.wunderground.com/api/#{Oblong.wunderground_api_key}/conditions/q/52.2140,0.1257.json"
+    #@wunderground_response = open(wunderground_url) { |f| f.read }
+    #@current_weather = @wunderground_response['current_observation']
+    @current_weather = {"temp_c" => "10.6", "feelslike_c" => "10.6", "forecast_url" => "http://icons-ak.wxug.com/i/c/k/nt_clear.gif"}
+
     render 'dashboard/index'
   end
 end
